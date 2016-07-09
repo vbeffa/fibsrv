109a95beb726ee951defa61caca224fabc0043a2
Vlad Beffa <vlad.beffa@originate.com>
2016-07-08 21:13:11 -0700

    memoize in all environments

--- app/services/ApplicationTimer.scala
+++ app/services/ApplicationTimer.scala
@@ -10,34 +10,32 @@ import util.{ConfigUtils, LoggingUtils}
 import scala.concurrent.Future
 
 /**
- * This class demonstrates how to run code when the
- * application starts and stops. It starts a timer when the
- * application starts. When the application stops it prints out how
- * long the application was running for.
- *
- * This class is registered for Guice dependency injection in the
- * [[Module]] class. We want the class to start when the application
- * starts, so it is registered as an "eager singleton". See the code
- * in the [[Module]] class to see how this happens.
- *
- * This class needs to run code when the server stops. It uses the
- * application's [[ApplicationLifecycle]] to register a stop hook.
- */
+  * This class demonstrates how to run code when the
+  * application starts and stops. It starts a timer when the
+  * application starts. When the application stops it prints out how
+  * long the application was running for.
+  *
+  * This class is registered for Guice dependency injection in the
+  * [[Module]] class. We want the class to start when the application
+  * starts, so it is registered as an "eager singleton". See the code
+  * in the [[Module]] class to see how this happens.
+  *
+  * This class needs to run code when the server stops. It uses the
+  * application's [[ApplicationLifecycle]] to register a stop hook.
+  */
 @Singleton
-class ApplicationTimer @Inject() (clock: Clock,
-                                  appLifecycle: ApplicationLifecycle,
-                                  app: Application,
-                                  fibSrv: FibonacciService,
-                                  implicit val config: Configuration) {
+class ApplicationTimer @Inject()(clock: Clock,
+                                 appLifecycle: ApplicationLifecycle,
+                                 app: Application,
+                                 fibSrv: FibonacciService,
+                                 implicit val config: Configuration) {
 
   // This code is called when the application starts.
   private val start: Instant = clock.instant
   Logger.info(s"Starting application at $start.")
-  if (app.mode == Mode.Dev) {
-    LoggingUtils.logMemoryUsage()
-    fibSrv.memoize(ConfigUtils.maxFibInput, ConfigUtils.maxFibListInput)
-    LoggingUtils.logMemoryUsage()
-  }
+  LoggingUtils.logMemoryUsage()
+  fibSrv.memoize(ConfigUtils.maxFibInput, ConfigUtils.maxFibListInput)
+  LoggingUtils.logMemoryUsage()
 
   // When the application starts, register a stop hook with the
   // ApplicationLifecycle object. The code inside the stop hook will
